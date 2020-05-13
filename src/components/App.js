24e493de619c4e04bd2b6dc59981beef8adbf95d
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-05-13 11:00:35 -0400

    initial commit and setup complete

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function decrease() {
+    return setCount(count - 1);
+  }
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <h1>REACT (useState Hook): Counter</h1>
+      </div>
+
+      <div className="row">
+        <button onClick={decrease}>-</button>
+        <div className="counter">
+          <h1>{count}</h1>
+        </div>
+        <button onClick={increase}>+</button>
+      </div>
+
+      <div className="row">
+        <button className="reset-btn" onClick={reset}>
+          RESET
+        </button>
+      </div>
+    </div>
+  );
+}
